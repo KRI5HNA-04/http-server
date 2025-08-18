e43647e5f5917848664298ce92b9260d24a2bc19
RS Krishna <king181219@gmail.com>
2025-08-18 19:15:12 +0530

    route management

--- index.js
+++ index.js
@@ -3,9 +3,18 @@ const fs = require("fs");
 const { strict } = require("assert");
 
 const myServer = http.createServer((req, res) => {
-  const log = `${Date.now()}: New request received\n`;
+  const log = `${Date.now()}: ${req.url} New request received\n`;
   fs.appendFile("log.txt", log, (err, data) => {
-    res.end("Hello from server");
+    switch (req.url) {
+      case "/":
+        res.end("This is Home Page");
+        break;
+      case "/about":
+        res.end("This is About Page");
+        break;
+      default:
+        res.end("404 not found");
+    }
   });
 });
 
